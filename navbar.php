e412f660d54256ac7022f4eb3a3427a548c1bc2b
Kemz <afilfriend@gmail.com>
2024-09-07 15:30:43 +0800

    add route

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <?php session_start(); ?>
 <nav class="navbar navbar-expand-lg bg-body-tertiary mx-auto w-50">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">User Management</a>
+        <a class="navbar-brand" href="dashboard.php">User Management</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
