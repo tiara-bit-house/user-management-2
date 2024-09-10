f3e65156500ee3fa29e258b12fb8c53c132f0fd4
Kemz <afilfriend@gmail.com>
2024-09-10 15:24:43 +0800

    add unique trait in email

--- app/proses_register.php
+++ app/proses_register.php
@@ -6,44 +6,49 @@ session_start();
 if (isset($_POST) && hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])) {
     $_SESSION['csrf_token'] =  bin2hex(random_bytes(32));
 
-    $name = $_POST['name'];
-    $email = htmlspecialchars($_POST['email'], ENT_QUOTES, 'utf-8');
-    $password = htmlspecialchars($_POST['password'], ENT_QUOTES, 'utf-8');
+    try {
+        $name = $_POST['name'];
+        $email = htmlspecialchars($_POST['email'], ENT_QUOTES, 'utf-8');
+        $password = htmlspecialchars($_POST['password'], ENT_QUOTES, 'utf-8');
 
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
 
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        header('Location: ../register.php?message="Email tidak valid"');
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            header('Location: ../register.php?message="Email tidak valid"');
+            exit();
+        }
+
+        $password = password_hash($password, PASSWORD_BCRYPT);
+
+        if (!$_FILES['image']['size']) {
+            header('Location: ../register.php?message="Gambar wajib diisi"');
+            exit();
+        }
+
+        $fileName = "photo_profiles/" . $_FILES['image']['name'];
+        $fileExt = pathinfo($fileName, PATHINFO_EXTENSION);
+
+        $typeGambar = ['jpg', 'jpeg', 'png'];
+
+        if (!in_array($fileExt, $typeGambar)) {
+            header('Location: ../register.php?message="File bukan berbentuk gambar"');
+            exit();
+        }
+
+        move_uploaded_file($_FILES['image']['tmp_name'], $fileName);
+
+        $stmt = $conn->prepare("INSERT INTO users (name, email, password, image_url)
+        VALUES (?,?,?,?)");
+        $stmt->bind_param('ssss', $name, $email, $password, $fileName);
+
+        if (!$stmt->execute()) {
+            header('Location: ../register.php?message="gagal register"');
+        } else {
+            header('Location: ../register.php?message="berhasil register"');
+        }
+    } catch (Exception $e) {
+        header('Location: ../register.php?message="Gagal register, kemungkinan email duplikat"');
         exit();
-    }
-
-    $password = password_hash($password, PASSWORD_BCRYPT);
-
-    if (!$_FILES['image']['size']) {
-        header('Location: ../register.php?message="Gambar wajib diisi"');
-        exit();
-    }
-
-    $fileName = "photo_profiles/" . $_FILES['image']['name'];
-    $fileExt = pathinfo($fileName, PATHINFO_EXTENSION);
-
-    $typeGambar = ['jpg', 'jpeg', 'png'];
-
-    if (!in_array($fileExt, $typeGambar)) {
-        header('Location: ../register.php?message="File bukan berbentuk gambar"');
-        exit();
-    }
-
-    move_uploaded_file($_FILES['image']['tmp_name'], $fileName);
-
-    $stmt = $conn->prepare("INSERT INTO users (name, email, password, image_url)
-    VALUES (?,?,?,?)");
-    $stmt->bind_param('ssss', $name, $email, $password, $fileName);
-
-    if (!$stmt->execute()) {
-        header('Location: ../register.php?message="gagal register"');
-    } else {
-        header('Location: ../register.php?message="berhasil register"');
     }
 } else {
     header('Location: ../register.php?message="CSRF token invalid"');
--- dashboard.php
+++ dashboard.php
@@ -23,6 +23,10 @@
         <p>Login sebagai : <?php echo $_SESSION['is_admin'] ? 'Admin' : 'Member' ?></p>
     </h1>
 
+    <div class="mx-auto w-50">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d75047.7985616875!2d115.20382537611451!3d-8.671183400150856!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd24149216c058f%3A0x5d8dd5076a7a4d06!2sLapangan%20Puputan%20Badung%20(I%20Gusti%20Ngurah%20Made%20Agung)!5e0!3m2!1sen!2sid!4v1725952243275!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+
 
 </body>
 
